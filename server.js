950c6412d0d95a09683e6daf1531999b9d64d8a3
march23sun <march23@gmail.com>
2016-07-02 20:03:17 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ var express = require("express"),
     io = require('socket.io').listen(server);
  
 server.listen(8080);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
