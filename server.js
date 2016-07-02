9c91b5b3d05d5bc64596e9e1875eb80b361d9381
march23sun <march23@gmail.com>
2016-07-02 20:01:43 +0800

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require("express"),
+    app = express(),
+    http = require("http"),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
+ 
+server.listen(8080);
