1c8171d6415df1416482ec00511785ed0c4bde24
march23sun <march23@gmail.com>
2016-07-02 20:15:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,29 @@ var express = require("express"),
     io = require('socket.io').listen(server);
  
 server.listen(8080);
+console.log('server running!');
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+ 
+    socket.on('send', function (data) {
+ 
+        switch ( data.act )
+        {
+            case "enter":
+            io.sockets.emit('get_response', data);
+            console.log("Sending getEnter");
+            break;
+            
+            case "changebg":
+            io.sockets.emit('get_response', data);
+            console.log("Sending changeBg");
+            break;
+        }
+ 
+    });
+ 
 });
